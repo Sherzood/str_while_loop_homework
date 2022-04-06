44c39fcde27ada2dfee6fecd919e294725be5ddd
sherzod89 <sherzodms@inbox.ru>
2022-04-06 22:18:42 +0300

    01

--- While02.py
+++ While02.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    count_letters=0
+    i=0
+    while i<len(s):
+        if s[i].isalpha():
+            count_letters+=1
+        i+=1    
+    return count_letters
+print(main('sherzod1989'))    
