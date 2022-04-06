4b806da38f701b526b9128715797887ff07eebfe
sherzod89 <sherzodms@inbox.ru>
2022-04-06 23:42:03 +0300

    01

--- While10.py
+++ While10.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    sum_odd_digits=0
+    i=0
+    while i<len(s):
+        if s[i].isdigit():
+            if int(s[i])%2!=0:
+                sum_odd_digits+=int(s[i])
+        i+=1        
+    return sum_odd_digits
