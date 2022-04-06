12f88ef2fcd4b8f173867e6ca747969391e6ed5f
sherzod89 <sherzodms@inbox.ru>
2022-04-06 22:42:20 +0300

    01

--- While04.py
+++ While04.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    count_u=0
+    i=0
+    while i<len(s):
+        if s[i].isupper():
+            count_u+=1
+        i+=1    
+    return count_u
+print(main('SHERzod34'))    
